fbfbbda0dc0a34917c1d6a5181f728a72076926b
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2023-08-04 19:32:19 +0600

    Save Barbershop

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
-#Получение данных
+
+#Получить данные
 get '/' do
   erb :index
+end
+
+#Отправить данные
+post '/' do
+  @name = params['name']
+  @phone_number = params['phone_number']
+  @date_time = params['date_time']
+
+  @title = "Thank you!"
+  @message = "Dear #{@name}, we'll be waiting for you at #{@date_time}."
+
+  f = File.open 'users.txt', 'a'
+  f.write "User: #{@name}, Phone: #{@phone_number}, Date and Time: #{@date_time}\n"
+  f.close
+
+  erb :message
+
 end
